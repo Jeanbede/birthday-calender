a9a9625766fb4fb112f7998a25087fa5159bbd9c
Jeanbede <njeanbeda@gmail.com>
2020-12-14 02:13:32 +0300

    adding the bootstrap file

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// function showDate() {
+    // document.getElementById("showdate") . innerHTML = Date();
+// }
+{/* <button type="button" onclick="showDate()">show Date</button> */}
+
+
+
+
+// const birthday = new Date('August 19, 1975 23:15:30');
+// const day1 = birthday.getDay();
+// Sunday - Saturday : 0 - 6
+
+// console.log(day1);
+// expected output: 2
+
+
+var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwane";]
+var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var days=["monday ","tuesday", "wednesday", "thursday ","friday", "saturday" , "sunday"];
+
+const birthday = new Date('August 19, 1975 23:15:30');
+const day1 = birthday.getDay();
+// Sunday - Saturday : 0 - 6
+
+console.log(day1);
+// expected output: 2
+
+
+function days(){
+    var maleName, femaleName, male, female;
+    maleName=
+}
