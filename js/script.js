caa11b3b8f4b52d52692725ec5a5dd2587a7a90a
Jeanbede <njeanbeda@gmail.com>
2020-12-14 03:04:51 +0300

    adding a readme content on the web

--- js/script.js
+++ js/script.js
@@ -1,32 +1,23 @@
-// function showDate() {
-    // document.getElementById("showdate") . innerHTML = Date();
-// }
-{/* <button type="button" onclick="showDate()">show Date</button> */}
 
-
-
-
-// const birthday = new Date('August 19, 1975 23:15:30');
-// const day1 = birthday.getDay();
-// Sunday - Saturday : 0 - 6
-
-// console.log(day1);
-// expected output: 2
 
 
 var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwane";]
 var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var days=["monday ","tuesday", "wednesday", "thursday ","friday", "saturday" , "sunday"];
+function birthDays("month","date","year"){
+    var birthYear =["month","date","year"]().getFullDate();
+    return birthday= ("maleName",femaleName");
+  if  results.display("your birthday is: maleName");
+    else;
+    results.display("your birthday is: femaleName");
 
-const birthday = new Date('August 19, 1975 23:15:30');
-const day1 = birthday.getDay();
-// Sunday - Saturday : 0 - 6
-
-console.log(day1);
-// expected output: 2
-
-
-function days(){
-    var maleName, femaleName, male, female;
-    maleName=
 }
+
+
+
+
+
+
+
+
+
