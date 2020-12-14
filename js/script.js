107d685b28ec52dfe987d26c59d12aa6888ddbba
Jeanbede <njeanbeda@gmail.com>
2020-12-14 09:40:24 +0300

    adding file on the web

--- js/script.js
+++ js/script.js
@@ -4,14 +4,39 @@
 var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwane";]
 var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var days=["monday ","tuesday", "wednesday", "thursday ","friday", "saturday" , "sunday"];
-function birthDays("month","date","year"){
-    var birthYear =["month","date","year"]().getFullDate();
-    return birthday= ("maleName",femaleName");
-  if  results.display("your birthday is: maleName");
-    else;
-    results.display("your birthday is: femaleName");
 
-}
+function checkbirthDay() {
+    return ()= days;
+  }
+  
+  function myFunction() {
+    document.getElementById("").innerHTML = days.findIndex(checkbirthDay);
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function birthDay("month","date","year"){
+    // var birthYear =["month","date","year"]().getFullDate();
+    // return birthday= ("maleName",femaleName");
+//   if  results.display("your birthday is: maleName");
+    // else;
+    // results.display("your birthday is: femaleName");
+
+// }
+
 
 
 
