3354cc2387dfe933460ecd207965319c4001abaa
Jeanbede <njeanbeda@gmail.com>
2020-12-15 20:48:33 +0300

    adding modifications to website

--- js/script.js
+++ js/script.js
@@ -1,65 +1,83 @@
+let yearEntered=document.getElementById("years").value
+if( yearEntered<1900){
+    alert("Not Apllicable")
+}
 
-  
 
-  function MakeArray(n) {
+
+function ShowArray(n) {
     this.length = n;
-    for (var i = 1; i <=n; i++) {
-      this[i] = 0;
+    for (var i = 1; i <= n; i++) {
+        this[i] = 0;
     }
- }
- day = new MakeArray(7);
- days[0] = "Saturday"
- days[1] = "Sunday"
- days[2] = "Monday"
- days[3] = "Tuesday"
- days[4] = "Wednesday"
- days[5] = "Thursday"
- days[6] = "Friday"
- months = new MakeArray(12);
- months[1] = "January"
- months[2] = "February"
- months[3] = "March"
- months[4] = "April"
- months[5] = "May"
- months[6] = "June"
- months[7] = "July"
- months[8] = "August"
- months[9] = "September"
- months[10] = "October"
- months[11] = "November"
- months[12] = "December"
- function compute(form) {
-    var val1 = parseInt(form.day.value, 10)
-    if ((val1 < 0) || (val1 > 31)) {
-       alert("The date you enterd is Invalid")
+
+}
+day = new ShowArray(7);
+days[0] = "Saturday"
+days[1] = "Sunday"
+days[2] = "Monday"
+days[3] = "Tuesday"
+days[4] = "Wednesday"
+days[5] = "Thursday"
+days[6] = "Friday"
+months = new ShowArray(12);
+months[1] = "January"
+months[2] = "February"
+months[3] = "March"
+months[4] = "April"
+months[5] = "May"
+months[6] = "June"
+months[7] = "July"
+months[8] = "August"
+months[9] = "September"
+months[10] = "October"
+months[11] = "November"
+months[12] = "December"
+function compute() {
+let dateEntered=document.getElementById("date").value
+let monthsEntered=document.getElementById("months").value
+let yearEntered=document.getElementById("years").value
+if( yearEntered<1900){
+    alert("Not Apllicable")
+    else{      
+    form.result1.value = yearEntered 
     }
-    var val2 = parseInt(form.months.value, 10)
-    if ((val2 < 0) || (val2 > 12)) {
-       alert("Invalid Month")
-    }
-    var val2x = parseInt(form.months.value, 10)
-    var val3 = parseInt(form.years.value, 10)
-    if (val3 < 1700) {
-       alert("???????")
-    }
-    if (val2 == 1) {
-       val2x = 13;
-       val3 = val3-1
-    }
-    if (val2 == 2) {
-       val2x = 14;
-       val3 = val3-1
-    }
-    var val4 = parseInt(((val2x+1)*3)/5, 10)
-    var val5 = parseInt(val3/4, 10)
-    var val6 = parseInt(val3/100, 10)
-    var val7 = parseInt(val3/400, 10)
-    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-    var val9 = parseInt(val8/7, 10)
-    var val0 = val8-(val9*7)
-    form.result1.value = months[val2]+" "+form.day.value +", "+form.years.value
-    form.result2.value = day[val0]
- }
+
+
+
+
+
+//     var val1 = parseInt(form.day.value, 10)
+//     if ((val1 < 0) || (val1 > 31)) {
+//         alert("")
+//     }
+//     var val2 = parseInt(form.months.value, 10)
+//     if ((val2 < 0) || (val2 > 12)) {
+//         alert("")
+//     }
+//     var val2x = parseInt(form.months.value, 10)
+//     var val3 = parseInt(form.years.value, 10)
+//     if (val3 < 1700) {
+//         alert("")
+//     }
+//     if (val2 == 1) {
+//         val2x = 13;
+//         val3 = val3 - 1
+//     }
+//     if (val2 == 2) {
+//         val2x = 14;
+//         val3 = val3 - 1
+//     }
+//     var val4 = parseInt(((val2x + 1) * 3) / 5, 10)
+//     var val5 = parseInt(val3 / 4, 10)
+//     var val6 = parseInt(val3 / 100, 10)
+//     var val7 = parseInt(val3 / 400, 10)
+//     var val8 = val1 + (val2x * 2) + val4 + val3 + val5 - val6 + val7 + 2
+//     var val9 = parseInt(val8 / 7, 10)
+//     var val0 = val8 - (val9 * 7)
+//     form.result1.value = months[val2] + " " + form.day.value + ", " + form.years.value
+//     form.result2.value = day[val0]
+// }
 
 
 
